1e305d6c2f3e5c2a5605df65998796a8a049485b
Anderson Fernandes <aasf_86@hotmail.com>
2018-03-15 23:46:09 -0300

    init

---
+++ app/Http/Controllers/TesteController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class TesteController extends Controller
+{
+
+    public function printTop(){
+        return '<h1>@Anderson Fernandes</h1>';
+    }
+}
+
